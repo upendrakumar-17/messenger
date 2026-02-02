cdc6b68cc82f57d94df128c92b10bf35ddc5142d
Upendra Kumar <ukupendrakumar1712@gmail.com>
2026-02-02 23:28:03 +0530

    first commit

---
+++ src/pages/ChatPage.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './style/ChatPage.css'
+
+const ChatPage = () => {
+  return (
+    <div className='chatpage'>
+        <div className='sidebar'>
+
+        </div>
+        <div className='chatwindow'>
+            <div className='chatheader'>
+
+            </div>
+            <div className='chatbody'>  
+                <div className='messagearea'>
+
+                </div>
+                <div className='inputarea'>
+
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ChatPage
